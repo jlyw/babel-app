e437041d9de5ce424bcccf8debfd867014cf7163
Jean Law Yim Wan <Jean Law Yim Wan>
2016-12-13 21:10:44 +0100

    Add bottom nav bar, change activity extends on MainActivity

--- app/src/main/java/com/babel/mybabelapplication/MainActivity.java
+++ app/src/main/java/com/babel/mybabelapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.babel.mybabelapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_home:
+
+                                break;
+                            case R.id.action_new_list:
+
+                                break;
+                            case R.id.action_profile:
+
+                                break;
+                        }
+                        return false;
+                    }
+                });
     }
 }
